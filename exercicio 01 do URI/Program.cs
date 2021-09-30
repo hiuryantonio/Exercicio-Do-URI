6076b07615fcf570582417bb17dae8d82dfe80b1
Hiury Gomes <hiury_jp@hotmail.com>
2021-09-30 10:02:49 -0300

    Exercicio 01 do URI

---
+++ exercicio 01 do URI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercicio_01_do_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, X;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            X = A + B;
+
+            Console.WriteLine("X = " + X);
+        }
+    }
+}
